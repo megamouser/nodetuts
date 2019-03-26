9088ef4efc2df33954755b543519ffb9cd8ce0e7
Artem <artemon-yrev@yandex.ru>
2019-03-26 16:58:15 +0300

    5.9

---
+++ 5.fs/7.example/app.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+fs.unlink('tutorial/example.txt', (err) => {
+    if(err) {
+        console.log(err);
+    } else {
+        fs.rmdir('tutorial', (err) => {
+            if(err) {
+                console.log(err);
+            } else {
+                console.log('deleted folder');
+            }
+        });
+    }
+});

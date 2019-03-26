df5c31151fab62f9f9daa66d82f6863822ff1f23
Artem <artemon-yrev@yandex.ru>
2019-03-26 14:52:17 +0300

    5.2

--- 5.fs/app.js
+++ 5.fs/app.js
@@ -7,7 +7,7 @@ fs.writeFile('example.txt', 'this is an example', (err) => {
         console.log(err);
     } else {
         console.log('File successfully created');
-        fs.readFile('example.txt', (err, file) => {
+        fs.readFile('example.txt', 'utf8', (err, file) => {
             if(err) {
                 console.log(err);
             } else {

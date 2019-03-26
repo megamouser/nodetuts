df9483d73a1222a880bfa0332d962ffa4a59a5ee
Artem <artemon-yrev@yandex.ru>
2019-03-26 17:33:05 +0300

    6.1

--- 6.streams/app.js
+++ 6.streams/app.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const readStream = fs.createReadStream('example.txt');
+const readStream = fs.createReadStream('example.txt', 'utf8');
 readStream.on('data', (chunk) => {
     console.log(chunk);
 });

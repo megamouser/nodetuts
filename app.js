27c9f6a88d6b4fbe091a9f33e5c23541c67e2632
Artem <artemon-yrev@yandex.ru>
2019-03-27 18:17:49 +0300

    9.3

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const app = express();
 
 app.use('/public', express.static(path.join(__dirname, 'static')));
+app.use('/public', express.static(path.join(__dirname, 'node_modules', 'bootstrap', 'dist')));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
     console.log(path.join(__dirname, 'static', 'index.html'));
+    console.log(path.join(__dirname, 'node_modules', 'bootstrap', 'dist'));
     res.sendFile(path.join(__dirname, 'static', 'index.html'));
+});
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+    
+    // database work here
+    res.send('successfully posted data');
 });
 
 app.listen(3000);

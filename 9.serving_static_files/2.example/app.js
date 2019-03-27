41854f84f20f97c8900d2e5f33c7604f57985fd0
Artem <artemon-yrev@yandex.ru>
2019-03-27 13:16:45 +0300

    8.3

---
+++ 9.serving_static_files/2.example/app.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const fs = require('fs');
+http.createServer((req, res) => {
+    const readStream = fs.createReadStream('static/example.json');
+    res.writeHead(200, { 'Content-type': 'application/json' });
+    readStream.pipe(res);
+}).listen(3000);
+
+
+

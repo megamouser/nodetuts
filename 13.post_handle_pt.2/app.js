14321637dc1287683f3064acc062b2eeb1a7b491
Artem Yurev <artemon-yrev@yandex.ru>
2019-03-30 17:36:03 +0300

    13.0

---
+++ 13.post_handle_pt.2/app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const Joi = require('joi');
+const app = express();
+
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.post('/', function(req, res) {
+    console.log(req.body);
+    //res.send('response from server: success');
+    
+    // const userDataSchema = Joi.object().keys({
+    //     email: Joi.string().trim().email().required(),
+    //     password: Joi.string().trim().required()
+    // });
+
+    // Joi.validate(req.body.user, userDataSchema, (err, result) => {
+    //     console.log(req.body);
+    //     if(err) {
+    //         console.log(err);
+    //         res.send('data posting error');
+    //     } else {
+    //         console.log(result);
+    //         res.send('successfully posted data');
+    //     }
+    // });
+
+});
+
+app.listen(3000);

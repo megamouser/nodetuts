a8ccd1cf2ce4c63fe518b812d0dbfc309fa3b52f
Artem Yurev <artemon-yrev@yandex.ru>
2019-03-31 01:53:08 +0300

    14.1

--- 14.node_router/routes/web.js
+++ 14.node_router/routes/web.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const route = express.Router();
 
+route.use((req, res, next) => {
+    console.log('middleware being used');
+    next();
+});
+
 route.get('/', (req, res) => {
     res.send('/ being hit');
 });

6517918c906698a60138ee1e6833d08bb8af5924
Artem Yurev <artemon-yrev@yandex.ru>
2019-03-30 23:23:52 +0300

    9.13

--- app.js
+++ app.js
@@ -7,28 +7,21 @@ const app = express();
 
 app.use('/public', express.static(path.join(__dirname, 'static')));
 app.use('/public', express.static(path.join(__dirname, 'node_modules', 'bootstrap', 'dist')));
+
+app.set('view engine', 'ejs');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-//app.set('view engine', 'ejs');
-
-//app.get('/:userQuery', (req, res) => {
-    //console.log(path.join(__dirname, 'static', 'index.html'));
-    //console.log(path.join(__dirname, 'node_modules', 'bootstrap', 'dist'));
-    //res.sendFile(path.join(__dirname, 'static', 'index.html'));
-    //res.render('index', { data:  { userQuery: req.params.userQuery, searchResults: ['book1', 'book2', 'book3'] }});
-//});
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'static', 'index.html'));    
 });
 
-app.post('/', (req, res) => {
-    //console.log(req.body);
-    // let keys = _.map(req.body, 'name');
-    // let values = _.map(req.body, 'value');
-    // let requestData = Object.assign({}, ...keys.map((n, index) => ({[n]: values[index]})));
+app.get('/:userQuery', (req, res) => {
+    res.render('index', {data: {userQuery: req.params.userQuery}});
+});
 
+app.post('/', (req, res) => {
     const schema = Joi.object().keys({
         email: Joi.string().trim().email().required(),
         password: Joi.string().min(5).max(10).required(),
